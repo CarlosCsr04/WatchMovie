e5b07cb9a00f0760c6cdf1277cd20601f9b041a7
CarlosCsr04 <117779472+CarlosCsr04@users.noreply.github.com>
2023-11-24 21:37:10 -0300

    atualização do projeto

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { listAll, ref, getDownloadURL, Storage } from '@angular/fire/storage';
 
+import { doc, collection, setDoc, Firestore, collectionData, getDocs} from '@angular/fire/firestore';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -9,7 +11,10 @@ import { listAll, ref, getDownloadURL, Storage } from '@angular/fire/storage';
   
 })
 export class Tab1Page {
+
   ficcao:any=[]
+  filmes:any=[]
+  presentingElement:any = null;
   ngOnInit() {
     listAll(ref(this.af, 'Ficção Científica')).then(imgs => {
       imgs.items.forEach((im) => {
@@ -19,9 +24,20 @@ export class Tab1Page {
         })
       })
     })
+
+
+      this.presentingElement = document.querySelector('.ion-page');
+
+      this.listar()
+  }
+  async listar(){
+    const querySapshop=await getDocs(collection(this.firestore, 'acao'))
+    querySapshop.forEach((doc)=>{
+      console.log(`${doc.id} => ${doc.data()['titulo']}`)
+      this.filmes=[...this.filmes,{titulo:doc.data()['titulo'],descricao:doc.data()['descricao'],categoria:doc.data()['categoria'],imagem:doc.data()['imagem']}]
+    })
   }
 
-
-  constructor(private af:Storage) {}
+  constructor(private af:Storage, private firestore:Firestore) {}
 
 }
